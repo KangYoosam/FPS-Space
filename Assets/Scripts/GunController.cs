47e7b79aab0e2e12f7795158061a8e4c41f97fc6
kang yoosam <yooska14@gmail.com>
2017-03-06 21:55:27 +0900

    [add]Player can fire the gun

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+	[SerializeField] private GameObject fireParticle;
+	[SerializeField] private AudioClip shotSound;
+
+	private AudioSource audioSource;
+	private bool isEmpty;
+
+	// Use this for initialization
+	private void Start ()
+	{
+		isEmpty = false;
+		audioSource = this.GetComponent<AudioSource> ();
+	}
+
+	public void Shot (Vector3 hitPoint)
+	{
+		GameObject fire = Instantiate (fireParticle, hitPoint, Quaternion.identity);
+		audioSource.PlayOneShot (shotSound);
+
+		Destroy (fire, 0.5f);
+	}
+
+	public bool hasAny ()
+	{
+		return !isEmpty;
+	}
+}
