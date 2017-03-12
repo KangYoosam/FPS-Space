b2e9604ac28d33c44793cd78e930e0a29f7b15a4
kang yoosam <yooska14@gmail.com>
2017-03-12 14:39:34 +0900

    Revert "Revert "[add]UI""
    This reverts commit cbd4a86d8416edaa9d7b1e4072fad16608d7d106.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,13 +5,19 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+	[SerializeField] private Text timer;
+	[SerializeField] private Text point;
 	[SerializeField] private Text bullet;
 	[SerializeField] private Text magazine;
 	[SerializeField] private GunController gunController;
+	[SerializeField] private GameManager gameManager;
+	private float time;
 
 	// Update is called once per frame
 	void Update ()
 	{
+		timer.text = "Time : " + (90f - Time.time).ToString ("N1") + "s";
+		point.text = "Pt : " + gameManager.GetCurrentPoint ();
 		bullet.text = "Bullet : " + gunController.loadedBulletCount + "/" + gunController.loadableBulletCount;
 		magazine.text = "BulletBox : " + gunController.magazineCount;
 	}
